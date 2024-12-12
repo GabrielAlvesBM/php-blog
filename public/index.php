84b713b6de3363a394d06249e6f393ecf282026a
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-12 15:18:13 -0300

    chore: organize folders for MVC

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php 
   require_once __DIR__ . '/../config.php';
   
-  include __DIR__ . '/../templates/layout/header.php';
+  include __DIR__ . '/../src/view/layout/header.php';
 
-  include __DIR__ . '/../templates/main.php';
+  include __DIR__ . '/../src/view/main.php';
 
-  include __DIR__ . '/../templates/layout/footer.php';
+  include __DIR__ . '/../src/view/layout/footer.php';
 ?>
