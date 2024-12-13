4810f105f35207b224e86473467830b89979cad8
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-13 16:22:15 -0300

    add: favicon

--- src/view/layout/header.php
+++ src/view/layout/header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP</title>
   <link rel="stylesheet" href="./css/global.css">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
   <header>
