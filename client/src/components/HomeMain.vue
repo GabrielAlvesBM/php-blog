0a5688c94723accaf5cf5fb21368c28d257b95fd
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-13 19:42:55 -0300

    add: client(VueJS) and server(PHP)

---
+++ client/src/components/HomeMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>PHP Blog</h1>
+
+  <p>Um blog feito com <span>PHP</span> + <span>VueJS</span>, para estudar php e coloca-lo na web em uma VPS.</p>
+
+  <pre>Futuramente: &lt;Posts recentes></pre>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 25px;
+
+    background-color: var(--primary-color);
+  }
+
+  span {
+    background-color: var(--primary-color);
+  }
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HomeMain from '@/components/HomeMain.vue';
+</script>
+
+<template>
+  <main>
+    <HomeMain />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
