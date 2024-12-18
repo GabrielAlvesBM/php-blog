ef80db1ca59356a32a4eb5075926cdea99547a06
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-18 18:49:22 -0300

    feat: server paths configured

--- server/index.php
+++ server/index.php
@@ -4,4 +4,33 @@ header('Content-type: application/json');
 
 date_default_timezone_set("America/Sao_Paulo");
 
-echo "Hello, World!";
+
+if (isset($_GET['path'])) {
+  $path = explode("/", $_GET['path']);
+} else {
+  echo "Caminho não existe";
+  exit;
+}
+
+if (isset($path[0])) {
+  $api = $path[0];
+} else {
+  echo "Caminho não existe";
+  exit;
+}
+
+if (isset($path[1])) {
+  $action = $path[1];
+} else {
+  $action = '';
+}
+
+if (isset($path[2])) {
+  $param = $path[2];
+} else {
+  $param = '';
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+print_r($path);
