b1be6ea740ec0c1bc98d7b06a194f4be35d9b960
Gabriel Alves <gabrielalves6002@gmail.com>
2024-12-23 17:22:11 -0300

    feat: get list with id

--- server/api/posts/GET.php
+++ server/api/posts/GET.php
@@ -14,7 +14,32 @@ if ($action === 'list' && $param === '') {
 
   if ($resultado) {
     echo json_encode(["dados" => $resultado]);
+    exit;
   } else {
     echo json_encode(["dados" => 'Não existem dados para retornar.']);
+    exit;
   }
 } 
+
+if ($action === 'list' && $param !== '') {
+  if (!is_numeric($param)) {
+    echo json_encode(["ERRO" => 'O parâmetro deve ser um número inteiro.']);
+    exit;
+  }
+
+  $db = DB::connect();
+  $sql = $db->prepare("SELECT * FROM posts WHERE id = :id");
+
+  $sql->bindParam('id', $param, PDO::PARAM_INT);
+  $sql->execute();
+
+  $resultado = $sql->fetchObject();
+
+  if ($resultado) {
+    echo json_encode(["dados" => $resultado]);
+    exit;
+  } else {
+    echo json_encode(["dados" => 'Não existe dado para retornar.']);
+    exit;
+  }
+}
